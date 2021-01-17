0a42092c4cac9eaaa4fe1fc212831cb34ad85cfb
andrewbazua@gmail.com <andrewbazua@gmail.com>
2021-01-17 14:44:49 -0800

    feat: add project folders with initial files

---
+++ source/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <thread>   // Will be using threading to speed up iterations.
+#include <time.h>   // Used to track the amount of iterations per second
+
+
+int main() {
+
+    std::cout << "Hello World!" << std::endl;
+    
+    return 0;
+}
