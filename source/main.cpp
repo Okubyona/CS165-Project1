c64395aeb04a1a261fcd8ae6e3524b199fa64ff2
andrewbazua@gmail.com <andrewbazua@gmail.com>
2021-01-17 14:50:53 -0800

    feat: add md5 files from cryptopp

--- source/main.cpp
+++ source/main.cpp
@@ -2,11 +2,12 @@
 #include <string>
 #include <thread>   // Will be using threading to speed up iterations.
 #include <time.h>   // Used to track the amount of iterations per second
+#include "md5.h"
 
 
 int main() {
 
     std::cout << "Hello World!" << std::endl;
-    
+
     return 0;
 }
